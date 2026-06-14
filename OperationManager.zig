6f13438b4689f3589753bdfbb70bfe08f8c64838
Austin Kolongowski <kolowemployment@gmail.com>
2026-06-14 12:19:50 -0400

    Adding operation manager

--- OperationManager.zig
+++ OperationManager.zig
@@ -1,4 +1,6 @@
 const std = @import("std");
+const builtin = @import("builtin");
+const MODE = builtin.mode;
 const ArrayList = std.ArrayList;
 const HashMapType = std.AutoArrayHashMapUnmanaged;
 const Registry = @import("src/Registry.zig").Registry;
@@ -11,7 +13,7 @@ const EntityRecordType = @import("src/EntityRecord.zig").EntityRecord;
 const RecordIndex = Registry.RecordIndex;
 
 const Operation = enum {
-    createEnt,
+    addEnt,
     deleteEnt,
     addComp,
     removeComp,
@@ -20,110 +22,129 @@ const Operation = enum {
 pub fn OperationManager(comptime components: []const Component) type {
     const Storage = ComponentStorage(components);
     const EntityRecord = EntityRecordType(components);
-    const HashMap = HashMapType(RecordIndex, EntityRecord);
+    const RecordOperation = struct{ entity_record: EntityRecord, operation: Operation };
+    const HashMap = HashMapType(EntityId, RecordOperation);
     
     return struct {
         const Self = @This();
         allocator: std.mem.Allocator,
         entity_records: HashMap = .empty,
         
-        operation_queue: OperationQueue = undefined,
         storage: Storage = undefined,
 
         pub fn init(allocator: std.mem.Allocator) Self {
-            var self: Self = .{
+            return .{
                 .allocator = allocator,
-                .operation_queue = OperationQueue.init(allocator),
-                .storage = Storage.init(allocator),
+                .storage = .init(allocator),
+                .entity_records = .empty,
             };
-            
-            return self;
         }
 
         pub fn deinit(self: *Self) void {
-            self.operation_queue.deinit();
             self.storage.deinit();
             self.entity_records.deinit(self.allocator);
-            self.entity_records.deinit(self.allocator);
         }
 
-        pub fn addOperation(self: *Self, operation: Operation, component_data: anytype, record_data: EntityRecord.RecordData) !void {
+        pub fn addOperation(self: *Self, comptime operation: Operation, component_data: anytype, record_data: EntityRecord.RecordData) !void {
+            const ent_id = record_data.entity_id;
             var ent_record: EntityRecord = .init(record_data);
             
+            if(comptime operation != .addEnt) {
+                const res = try self.entity_records.getOrPut(self.allocator, record_data.entity_id);
+                if(res.found_existing) ent_record = res.value_ptr.entity_record;
+            }
+            
             switch (operation) {
-                .addEnt => self.appendAddOperation(&ent_record, component_data),
-                .deleteEnt => self.appendRemoveOperation(&ent_record),
-                // .addComp => ,
-                // .removeComp => ,
+                .addEnt => {
+                    if(comptime MODE == .Debug or MODE == .ReleaseSafe) {
+                        const found_ent = self.entity_records.get(record_data.entity_id);
+                        if(operation == .addEnt and found_ent != null) {
+                            std.debug.panic("Duplicate create entity operation found for EntityId {}\n", .{ent_id});
+                        }
+                    }
+                    
+                    self.appendAddOperation(.addEnt, &ent_record, component_data);
+                    const record_operation: RecordOperation = .{.entity_record = record_data.entity_id, .operation = operation};
+                    try self.entity_records.putNoClobber(self.allocator, ent_id, record_operation);
+                },
+                .addComp => {
+                    self.appendAddOperation(.addEnt, &ent_record, component_data);
+                    const record_operation: RecordOperation = .{.entity_record = record_data.entity_id, .operation = operation};
+                    try self.entity_records.put(self.allocator, ent_id, record_operation);
+                },
+                // .deleteEnt => self.appendRemoveOperation(&ent_record),
+                // // .removeComp => ,
             }
         }
 
-        fn appendAddOperation(self: *Self, entity_record: *EntityRecord, component_data: anytype) !void {
+        fn appendAddOperation(self: *Self, comptime operation: Operation, entity_record: *EntityRecord, component_data: anytype) !void {
+            if(operation != .addEnt or operation != .addComp) 
+                @compileError("Invalid Operation value for appendAddOperation function");
+
             inline for(std.meta.fields(@TypeOf(component_data))) |field| {
-                const comp_value = @field(component_data, field_name);
-                const comp_array = self.storage.getComponentArray(comp);
-                try @field(self.operation_queue, @tagName(Operation.add)).append(self.allocator, comp_value);
-            }
-            inline for(components) |comp| {
-                const field_name = @tagName(comp);
-                if(@hasField(@TypeOf(component_data), field_name)) {
-                    //@field
-                } 
+                const comp_tag = comptime CR.getEnumByName(field.name);
+                const comp_value = @field(component_data, field.name);
+                const comp_list = self.storage.getComponentArray(comp_tag);
+                try comp_list.append(self.allocator, comp_value);
+                entity_record.setComponentIndex(comp_tag, comp_list.items.len - 1);
             }
         }
         
-        fn appendRemoveOperation(self: *Self ,entity_record: *EntityRecord) void {
-            _ = self;
+        fn appendRemoveOperation(self: *Self, entity_record: *EntityRecord) void {
+            _ = self; _ = entity_record;
         }
-
-        fn append
     };
 }
 
-const InnerOperationQueue = blk: {
-    const operations = std.meta.tags(Operation);
-    var names: [operations.len][]const u8 = undefined;
-    var types: [operations.len]type = undefined;
-    var attrs: [operations.len]std.builtin.Type.StructField.Attributes = undefined;
+// const InnerOperationQueue = blk: {
+//     const operations = std.meta.tags(Operation);
+//     var names: [operations.len][]const u8 = undefined;
+//     var types: [operations.len]type = undefined;
+//     var attrs: [operations.len]std.builtin.Type.StructField.Attributes = undefined;
 
-    for (operations, 0..) |operation, i| {
-        names[i] = @tagName(operation);
-        types[i] = ArrayList(RecordIndex);
-        attrs[i] = .{};
-    }
+//     for (operations, 0..) |operation, i| {
+//         names[i] = @tagName(operation);
+//         types[i] = ArrayList(RecordIndex);
+//         attrs[i] = .{};
+//     }
 
-    break :blk @Struct(
-        .auto,
-        null,
-        &names,
-        &types,
-        &attrs,
-    );
-};
+//     break :blk @Struct(
+//         .auto,
+//         null,
+//         &names,
+//         &types,
+//         &attrs,
+//     );
+// };
 
-const OperationQueue = struct {
-    const Self = @This();
+// const OperationQueue = struct {
+//     const Self = @This();
 
-    allocator: std.mem.Allocator,
-    inner_storage: InnerOperationQueue = undefined,
+//     allocator: std.mem.Allocator,
+//     inner_storage: InnerOperationQueue = undefined,
 
-    pub fn init(allocator: std.mem.Allocator) Self {
-        var self: Self = .{ .allocator = allocator };
+//     pub fn init(allocator: std.mem.Allocator) Self {
+//         var self: Self = .{ .allocator = allocator };
 
-        inline for (std.meta.fields(InnerOperationQueue)) |field| {
-            @field(self.inner_storage, field.name) = .empty;
-        }
+//         inline for (std.meta.fields(InnerOperationQueue)) |field| {
+//             @field(self.inner_storage, field.name) = .empty;
+//         }
 
-        return self;
-    }
+//         return self;
+//     }
 
-    pub fn deinit(self: *Self) void {
-        inline for (std.meta.fields(InnerOperationQueue)) |field| {
-            @field(self.inner_storage, field.name).deinit(self.allocator);
-        }
-    }
+//     pub fn deinit(self: *Self) void {
+//         inline for (std.meta.fields(InnerOperationQueue)) |field| {
+//             @field(self.inner_storage, field.name).deinit(self.allocator);
+//         }
+//     }
 
-    pub fn getComponentArray(self: *Self, comptime operation: Operation) *ArrayList(RecordIndex) {
-        return &@field(self.inner_storage, @tagName(operation));
-    }
-};
+//     pub fn addToOperationArray(self: *Self, comptime operation: Operation, record_index: RecordIndex) !void {
+//         const op_array = self.getOperationArray(operation);
+//         try op_array.append(self.allocator, record_index);
+//     } 
+
+//     pub fn getOperationArray(self: *Self, comptime operation: Operation) *ArrayList(RecordIndex) {
+//         return &@field(self.inner_storage, @tagName(operation));
+//     }
+// };
